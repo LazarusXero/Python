2953be55721e83f279424e2f22bd476fff0f0108
Dave Leonard <davidpleonard@comcast.net>
2018-06-27 22:05:55 -0500

    finished LED Flashing script

--- led_flashing.py
+++ led_flashing.py
@@ -1,3 +1,15 @@
 #  This will be my LED flashing python script
 
 import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(24,GPIO.OUT,initial=GPIO.LOW)
+
+
+while True:
+    GPIO.output(24,GPIO.HIGH)
+    sleep(1)
+    GPIO.output(24,GPIO.LOW)
+    sleep(1)
